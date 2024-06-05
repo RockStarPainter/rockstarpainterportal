7419df7be6a34d70991e253a3a773756b513d2b5
ShabiGardezi <shabigardezi51214@gmail.com>
2024-06-05 21:21:30 +0500

    header design

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -136,6 +136,7 @@ const LoginPage = () => {
 
   const onSubmit = (data: FormData) => {
     const { email, password } = data
+
     // auth.login({ email, password, rememberMe }, () => {
     //   setError('email', {
     //     type: 'manual',
