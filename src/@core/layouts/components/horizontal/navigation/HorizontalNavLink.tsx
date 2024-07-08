9b4e1a13e4102eea7f8aafa78620307462119f2c
ShabiGardezi <shabigardezi51214@gmail.com>
2024-07-09 04:15:40 +0500

    horizontal and vertical nav link

--- src/@core/layouts/components/horizontal/navigation/HorizontalNavLink.tsx
+++ src/@core/layouts/components/horizontal/navigation/HorizontalNavLink.tsx
@@ -1,5 +1,5 @@
 // ** React Imports
-import { ElementType, Fragment } from 'react'
+import { ElementType } from 'react'
 
 // ** Next Imports
 import Link from 'next/link'
@@ -8,7 +8,8 @@ import { useRouter } from 'next/router'
 // ** MUI Imports
 import Box from '@mui/material/Box'
 import Chip from '@mui/material/Chip'
-import List from '@mui/material/List'
+
+// import List from '@mui/material/List'
 import { styled } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
 import ListItemIcon from '@mui/material/ListItemIcon'
@@ -27,7 +28,8 @@ import { Settings } from 'src/@core/context/settingsContext'
 // ** Custom Components Imports
 import UserIcon from 'src/layouts/components/UserIcon'
 import Translations from 'src/layouts/components/Translations'
-import CanViewNavLink from 'src/layouts/components/acl/CanViewNavLink'
+
+// import CanViewNavLink from 'src/layouts/components/acl/CanViewNavLink'
 
 // ** Util Imports
 import { hexToRGBA } from 'src/@core/utils/hex-to-rgba'
@@ -63,7 +65,9 @@ const ListItem = styled(MuiListItem)<
 
 const HorizontalNavLink = (props: Props) => {
   // ** Props
-  const { item, settings, hasParent } = props
+  // const { item, settings, hasParent } = props
+
+  const { item, hasParent } = props
 
   // ** Hook & Vars
   const router = useRouter()
@@ -71,7 +75,7 @@ const HorizontalNavLink = (props: Props) => {
 
   const icon = item.icon ? item.icon : navSubItemIcon
 
-  const Wrapper = !hasParent ? List : Fragment
+  // const Wrapper = !hasParent ? List : Fragment
 
   const isNavLinkActive = () => {
     if (router.pathname === item.path || handleURLQueries(router, item.path)) {
@@ -82,70 +86,66 @@ const HorizontalNavLink = (props: Props) => {
   }
 
   return (
-    <CanViewNavLink navLink={item}>
-      <Wrapper {...(!hasParent ? { component: 'div', sx: { py: settings.skin === 'bordered' ? 2.625 : 2.75 } } : {})}>
-        <ListItem
-          component={Link}
-          disabled={item.disabled}
-          {...(item.disabled && { tabIndex: -1 })}
-          className={clsx({ active: isNavLinkActive() })}
-          target={item.openInNewTab ? '_blank' : undefined}
-          href={item.path === undefined ? '/' : `${item.path}`}
-          onClick={e => {
-            if (item.path === undefined) {
-              e.preventDefault()
-              e.stopPropagation()
-            }
-          }}
-          sx={{
-            ...(item.disabled ? { pointerEvents: 'none' } : { cursor: 'pointer' }),
-            ...(!hasParent
-              ? {
-                  borderRadius: '8px',
-                  '&.active, &.active:hover': {
-                    backgroundColor: 'primary.main',
-                    '&:focus-visible': { backgroundColor: 'primary.dark' },
-                    '& .MuiTypography-root, & .MuiListItemIcon-root': {
-                      color: 'common.white'
-                    }
-                  }
+    <ListItem
+      component={Link}
+      disabled={item.disabled}
+      {...(item.disabled && { tabIndex: -1 })}
+      className={clsx({ active: isNavLinkActive() })}
+      target={item.openInNewTab ? '_blank' : undefined}
+      href={item.path === undefined ? '/' : `${item.path}`}
+      onClick={e => {
+        if (item.path === undefined) {
+          e.preventDefault()
+          e.stopPropagation()
+        }
+      }}
+      sx={{
+        ...(item.disabled ? { pointerEvents: 'none' } : { cursor: 'pointer' }),
+        ...(!hasParent
+          ? {
+              borderRadius: '8px',
+              '&.active, &.active:hover': {
+                backgroundColor: 'primary.main',
+                '&:focus-visible': { backgroundColor: 'primary.dark' },
+                '& .MuiTypography-root, & .MuiListItemIcon-root': {
+                  color: 'common.white'
                 }
-              : {
-                  '&.active, &.active:hover': {
-                    '&:focus-visible': {
-                      backgroundColor: theme => hexToRGBA(theme.palette.primary.main, 0.24)
-                    }
-                  }
-                })
+              }
+            }
+          : {
+              '&.active, &.active:hover': {
+                '&:focus-visible': {
+                  backgroundColor: theme => hexToRGBA(theme.palette.primary.main, 0.24)
+                }
+              }
+            })
+      }}
+    >
+      <Box sx={{ gap: 2, width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            ...(menuTextTruncate && { overflow: 'hidden' })
           }}
         >
-          <Box sx={{ gap: 2, width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-            <Box
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                ...(menuTextTruncate && { overflow: 'hidden' })
-              }}
-            >
-              <ListItemIcon sx={{ mr: hasParent ? 3 : 2.5, color: 'text.primary' }}>
-                <UserIcon icon={icon} fontSize={icon === navSubItemIcon ? '0.5rem' : '1.5rem'} />
-              </ListItemIcon>
-              <Typography {...(menuTextTruncate && { noWrap: true })}>
-                <Translations text={item.title} />
-              </Typography>
-            </Box>
-            {item.badgeContent ? (
-              <Chip
-                size='small'
-                label={item.badgeContent}
-                color={item.badgeColor || 'primary'}
-                sx={{ '& .MuiChip-label': { px: 2.5, lineHeight: 1.385, textTransform: 'capitalize' } }}
-              />
-            ) : null}
-          </Box>
-        </ListItem>
-      </Wrapper>
-    </CanViewNavLink>
+          <ListItemIcon sx={{ mr: hasParent ? 3 : 2.5, color: 'text.primary' }}>
+            <UserIcon icon={icon} fontSize={icon === navSubItemIcon ? '0.5rem' : '1.5rem'} />
+          </ListItemIcon>
+          <Typography {...(menuTextTruncate && { noWrap: true })}>
+            <Translations text={item.title} />
+          </Typography>
+        </Box>
+        {item.badgeContent ? (
+          <Chip
+            size='small'
+            label={item.badgeContent}
+            color={item.badgeColor || 'primary'}
+            sx={{ '& .MuiChip-label': { px: 2.5, lineHeight: 1.385, textTransform: 'capitalize' } }}
+          />
+        ) : null}
+      </Box>
+    </ListItem>
   )
 }
 
