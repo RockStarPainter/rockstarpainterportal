88f9a2d26bd3d0c8bbc2bfc0073bb5cfc48d54d5
ShabiGardezi <shabigardezi51214@gmail.com>
2024-07-26 03:33:37 +0500

    merging sms into main

--- src/pages/scheduling-dashboard/index.js
+++ src/pages/scheduling-dashboard/index.js
@@ -131,15 +131,15 @@ const Home = () => {
     }
   }
 
-  const handleSendSMS = async appointment => {
-    try {
-      await axios.post('/api/appointments/send-sms', { appointmentId: appointment._id })
-      toast.success('SMS sent successfully')
-    } catch (error) {
-      console.error('Error sending SMS:', error)
-      toast.error('Error sending SMS')
-    }
-  }
+  // const handleSendSMS = async appointment => {
+  //   try {
+  //     await axios.post('/api/appointments/send-sms', { appointmentId: appointment._id })
+  //     toast.success('SMS sent successfully')
+  //   } catch (error) {
+  //     console.error('Error sending SMS:', error)
+  //     toast.error('Error sending SMS')
+  //   }
+  // }
 
   const columns = useMemo(
     () => [
@@ -227,9 +227,9 @@ const Home = () => {
                 <SendIcon />
               </div>
               <div style={{ width: '15px' }}></div>
-              <div onClick={() => handleSendSMS(appointment)} style={{ cursor: 'pointer' }}>
+              {/* <div onClick={() => handleSendSMS(appointment)} style={{ cursor: 'pointer' }}>
                 <SmsIcon />
-              </div>
+              </div> */}
             </Box>
           )
         }
