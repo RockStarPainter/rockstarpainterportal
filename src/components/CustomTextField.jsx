de078bad3d9f8123d191dff66c2992d10486153a
ShabiGardezi <shabigardezi51214@gmail.com>
2024-07-31 02:29:33 +0500

    fixed again

--- src/components/CustomTextField.jsx
+++ src/components/CustomTextField.jsx
@@ -1,11 +1,14 @@
 import { Box, TextField, Typography } from '@mui/material'
 import React from 'react'
 import { Controller, useFormContext } from 'react-hook-form'
-import CheckCircleIcon from '@mui/icons-material/CheckCircle'
-import { green } from '@mui/material/colors'
+
+// import CheckCircleIcon from '@mui/icons-material/CheckCircle'
+// import { green } from '@mui/material/colors'
+
 function CustomTextField({ name, label, view, ...others }) {
   const methods = useFormContext()
   const { control, getValues } = methods
+
   return (
     <>
       {!view ? (
